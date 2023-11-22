fa1c3b9f24f1f77b4f7faa3b0229be18f648003f
jb3027 <146097465+jb3027@users.noreply.github.com>
2023-11-22 16:47:43 +0000

    Trying to figure it out

--- sketch.js
+++ sketch.js
@@ -3,93 +3,6 @@ function preload() {
     elf2 = loadImage("assets/pixil-frame-0 (1).png");
 }
 
-class Elf {
-    elf1;
-    elf2; 
-    #xDir;
-    #yDir;
-
-    constructor() {
-        
-    }
-
-    move() {
-        
-    }
-
-    goLeft() {
-        this.#xDir = 1;
-        this.#yDir = 0;
-    }
-
-    goRight() {
-        this.#xDir = -1;
-        this.#yDir = 0;
-    }
-
-    goUp() {
-        this.#xDir = 0;
-        this.#yDir = -1;
-    }
-
-    goDown() {
-        this.#xDir = 0;
-        this.#yDir = 1;
-    }
-
-    addSegment() {
-        let prevSegment = this.#segments[this.#segments.length - 1];
-        let newSegX = prevSegment.getX() - prevSegment.getWidth() * this.#xDir;
-        let newSegY = prevSegment.getY() - prevSegment.getWidth() * this.#yDir;
-        this.#segments.push(new Segment(newSegX, newSegY));
-    }
-
-    draw() {
-        fill(0, 255, 0);
-        for(let segment of this.#segments) {
-            segment.draw();
-        }
-    }
-
-    isOverFood(food) {
-        return this.#segments[0].isOverPoint(food.getX(), food.getY());
-    }
-
-    touchingWall() {
-        let currentSegment = this.#segments[0];
-        if((currentSegment.getX() === width || currentSegment.getX() < 0) || (currentSegment.getY() === height || currentSegment.getY() < 0)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * @returns {number} segments array
-     */
-    getSegments() {
-        return this.#segments;
-    }
-
-    /**
-     * @returns {number} xDir
-     */
-    getXDir() {
-        return this.#xDir;
-    }
-
-    /**
-     * @returns {number} yDir
-     */
-    getYDir() {
-        return this.#yDir;
-    }
-
-}
-
-
-
-
 function setup() {
     createCanvas(800, 600);
     rectMode(CENTER);
@@ -97,12 +10,10 @@ function setup() {
 }
 
 function draw() {
-    image(elf[0], 300, 300, 200, 200);
+    image(elf1, 300, 300, 200, 200);
+    image(elf2, 300, 300, 300, 300);
 
     
 }
 
-function running() {
-    
-}
 

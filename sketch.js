6ed2fdef56bd0cd0537a1010b41941f1f51cbef0
jb3027 <146097465+jb3027@users.noreply.github.com>
2023-11-22 21:11:12 +0000

    started again to stop fatal error - needs classes

---
+++ sketch.js
@@ -0,0 +1,18 @@
+let elf;
+
+function preload() {
+    elf = loadImage("assets/pixil-frame-0 (1).png");
+}
+
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    image(elf, 300, 300, 200, 200);
+
+    if(keyIsDown === RIGHT_ARROW) {
+        
+    }
+}
+
